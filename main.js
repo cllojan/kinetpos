1d4a5a1b5c1d8a36a2c8843144ca6bc16dc9bd25
Lummy <67608060+cllojan@users.noreply.github.com>
2022-06-30 20:53:51 +0000

    responsive

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ document.addEventListener("scroll", () => {
     let header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY>0);
 })
+
+let menuToggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () =>{
+    menu.classList.toggle("visible")
+})
